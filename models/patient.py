942163a3cf65ce976d6d7ba0069e5b45df8b49b7
HoangNam <hnam.19498@outlook.com>
2022-11-16 14:59:07 +0700

    Many2many Field In Odoo 15

--- models/patient.py
+++ models/patient.py
@@ -15,6 +15,7 @@ class HospitalPatient(models.Model):
     active = fields.Boolean(string='Active', default=True)
     appointment_id = fields.Many2one(comodel_name='hospital.appointment', string='Appointment')
     image = fields.Image(string='Image')
+    tag_ids = fields.Many2many('patient.tag', string='Tags')
 
     @api.depends("date_of_birth")
     def _compute_age(self):
