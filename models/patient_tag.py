808f0ddafbd1f8cbb8530f3d05846de2961b8b7c
HoangNam <hnam.19498@outlook.com>
2022-11-16 16:21:59 +0700

    Create Activity View In Odoo 15

--- models/patient_tag.py
+++ models/patient_tag.py
@@ -7,4 +7,5 @@ class PatientTag(models.Model):
 
     name = fields.Char(string="Name", required=True)
     active = fields.Boolean(string='Active', default=True)
-    color = fields.Char(string='Color')
+    color = fields.Integer(string='Color')
+    color2 = fields.Char(string='Color2')
