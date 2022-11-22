c602c97105d9b51518284da2cd5c2b67758a54ca
HoangNam <hnam.19498@outlook.com>
2022-11-22 15:08:38 +0700

    Target Inline In Odoo 15

---
+++ models/odoo_playground.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
+
+
+class OdooPlayground(models.Model):
+    _name = 'odoo.playground'
+    _description = 'Odoo Playground'
+
+    DEFAULT_ENV_VARIABLES = '''
+    1. A
+    2. B
+    3. C
+    4. D'''
+    model_id = fields.Many2one('ir.model', string='Model')
+    code = fields.Text(string='Code', default=DEFAULT_ENV_VARIABLES)
+    result = fields.Text(string="Result")
+
+    def action_execute(self):
+        try:
+            if self.model_id:
+                model = self.env[self.model_id.model]
+            else:
+                model = self
+            self.result = safe_eval(self.code.strip(), {'self': model})
+        except Exception as e:
+            self.result = str(e)
