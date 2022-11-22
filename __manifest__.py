90568d9a247121389151a57e42a4e6ff7695742d
HoangNam <hnam.19498@outlook.com>
2022-11-22 09:27:52 +0700

    Load Data From CSV File In Odoo 15

--- __manifest__.py
+++ __manifest__.py
@@ -9,11 +9,15 @@
     'depends': ['mail', 'product'],
     'data': [
         'security/ir.model.access.csv',
+        'data/patient_tag_data.xml',
+        'data/patient.tag.csv',
+        'wizard/cancel_appointment_view.xml',
         'views/menu.xml',
         'views/patient_view.xml',
         'views/patient_tag_view.xml',
         'views/female_patient_view.xml',
-        'views/kids_patient_view.xml',
         'views/appointment_view.xml',
     ],
+    'demo': [],
+    'license': 'LGPL-3',
 }
--- wizard/cancel_appointment.py
+++ wizard/cancel_appointment.py
@@ -4,4 +4,5 @@ from odoo import fields, models, api
 class CancelAppointmentWizard(models.TransientModel):
     _name = 'cancel.appointment.wizard'
     _description = 'Cancel Appointment Wizard'
-    apopointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
